94fb5125d0d49b9f2bee41aa8317360f8619c2d3
Tegar Dito Priandika <102775940+OmTegar@users.noreply.github.com>
2022-07-30 21:32:36 +0700

    Update v1

---
+++ python/main.py
@@ -0,0 +1,21 @@
+class mesin:
+    def __init__(self, dhc, security, off, exit , keseimbangan, kalori, diabetes, pernapasan, denyut, menu, intro ):
+        self.dhc = dhc
+        self.security = security
+        self.off = off
+        self.exit = exit
+        self.keseimbangan = keseimbangan
+        self.kalori = kalori
+        self.diabetes = diabetes 
+        self.pernapasan = pernapasan
+        self.denyut = denyut
+        self.menu = menu 
+        self.intro = intro
+
+        def dhc(self):
+            print('')
+            print("""
+                                >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+                                                            Daily Health Check !...
+                                <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                                                                                                                """)
